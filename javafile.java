b12eafb4083d05a339f4a4273dc21279c4e61f42
Murad Asgarov <muradaskarov1989@gmail.com>
2022-09-22 16:44:19 -0700

    code.txt file changed and added a new file

---
+++ javafile.java
@@ -0,0 +1,31 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int number; // declare a variable called number, type is integer
+
+        number = 65; // initialization (giving initial value)
+
+        int num = 45; // This is an example of declare and initialize in one line
+
+
+        // Variable has to be initialized before using
+
+        System.out.println(number+number); // we can also do operation by using initialized variables
+
+        // benefit of initialization is, it stores data to make it possible to use it later.
+
+
+
+        int age, height; // multiple declaration -> is possible only with same type of variable
+        // int num2, double num3 // different types are not allowed in same line
+        // int num4, int num5 // this will not work either, type of variable can be declared only once
+
+        int num2 = 5, num3, num4 = 45;  // this way is also possible.
+
+
+    }
+
+
+
+}
